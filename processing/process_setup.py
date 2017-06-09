d6e798e295ca8aaa953afd4c6878eec89ff14933
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-06-09 07:40:13 +0200

    close swift connection

--- processing/process_setup.py
+++ processing/process_setup.py
@@ -5,8 +5,8 @@ from control import openstack.OpenStackVMOperations
 def main():
     os = OpenStackVMOperations()
     os.readConf()
-    swift = os.swiftConn()
-    swift.put_container('test_container')
+    with os.swiftConn() as swift:
+        swift.put_container('test_container')
 
 if __name__ == "__main__":
     main()
