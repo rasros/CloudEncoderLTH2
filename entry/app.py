9b6cd769c216b626f9f57ad98f414c2bf7924269
Marten Lager <marten.lager@cs.lth.se>
2017-06-19 11:01:53 +0200

    Adding working app. Finally!

--- entry/app.py
+++ entry/app.py
@@ -6,17 +6,27 @@ from flask import jsonify
 import uuid
 import pika
 import sys
+import threading
 
+lock = threading.Lock()
 
 app = Flask(__name__)
+status_dict = {}
+status_channel = {}
+
+#initializing status queue
+status_connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+status_channel = status_connection.channel()
+status_channel.queue_declare(queue='status_queue', durable=True)
 
 @app.route('/',methods=['POST'])
 def index():
-	global task_queue_connection
+	global status_dict
+	global status_channel
 	videoFile = request.files['file'];
 	theID = uuid.uuid4()
 	videoFile.save(str(theID) + ".mp4")
-	
 	UUIDToBeConverted = str(theID)
 	task_queue_channel.basic_publish(exchange='',
 					  routing_key='task_queue',
@@ -24,26 +34,34 @@ def index():
 					  properties=pika.BasicProperties(
 						 delivery_mode = 2, # make message persistent
 					  ))
-	print(" [x] Sent request for converting %r" % UUIDToBeConverted)
-  
-	
+	print("  [x] Sent request for converting %r" % UUIDToBeConverted)
+
+	status_dict[str(theID)] = 1
 	resp = Response()
 	resp.headers['Location'] = '/' + str(theID)
 	return resp,201
 
-
-
 @app.route('/<uuid>/status')
 def status(uuid):
+	
 	global status_dict
-	progress = status_dict[uuid]
+	global status_channel#global status_dict
+	print "  STATUS: Length of dictionary: " + str(len(status_dict) )
+
+	if(status_dict.has_key(str(uuid))):
+		progress = status_dict[str(uuid)]
+	else:
+		print "  STATUS: dictionary has not: " + uuid
+		progress = 0
 	
 	if(progress == 0):
+		print "  STATUS: progress: " + str(progress)
 		res = { 'status' : 'QUEUED' , 'progress' : 0 }
 	elif(progress == 100):
 		res = { 'status' : 'DONE' , 'progress' : 100 }
 	else:
 		res = { 'status' : 'PROCESSING' , 'progress' : progress }
+	print "  Status respons for " + str(uuid) + " is: " + str(res)
 	return jsonify(res)
 
 
@@ -56,51 +74,37 @@ def download(uuid):
 	index = 0
 	return resp
 	
-
-status_dict = {}
-
 def callback(ch, method, properties, body):
+
 	global status_dict
+	global status_channel
+
+	print "  Callback: Received body:" + body
 	words = body.split()
 	uuid = words[0]
 	percentage = words[1]
-	processing_node = words[2]
-	status_dict[uuid] = percentage
-	print "The file with the UUID '" + uuid + "' is finished to " + percentage + "%." + 
-	ch.basic_ack(delivery_tag = method.delivery_tag)
+	status_dict[str(uuid)] = int(percentage)
+	ch.basic_ack(delivery_tag = method.delivery_tag) # should be first or last in callback function??????????????????????????
 
 def start_consum():
 	global status_channel
 	status_channel.start_consuming()
 
+status_channel.basic_qos(prefetch_count=1) #only sends one message at the time and waits for the ack
+status_channel.basic_consume(callback,
+                      queue='status_queue')
 
 if __name__ == '__main__':
-	
-	#initializing task_queue
 	task_queue_connection = pika.BlockingConnection(pika.ConnectionParameters(
 		host='localhost'))
 	task_queue_channel = task_queue_connection.channel()
 	task_queue_channel.queue_declare(queue='task_queue', durable=True)
 
-	#initializing status queue
-	status_connection = pika.BlockingConnection(pika.ConnectionParameters(
-		host='localhost'))
-	status_channel = status_connection.channel()
-	status_channel.queue_declare(queue='status_queue', durable=True)
-	
-	#starting status queue
-	print(' [*] Waiting for status messages. To exit press CTRL+C')
-	status_channel.basic_qos(prefetch_count=1) #only sends one message at the time and waits for the ack
-	status_channel.basic_consume(callback,
-					  queue='status_queue')
+	print('  [*] Waiting for status messages. To exit press CTRL+C')
+
 	t = threading.Thread(target = start_consum , args = [])
 	t.start()
+
+	app.run(debug=False) #DEBUG SHOULD ALWAYS BE FALSE!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	
-	app.run(debug=True)
-	
-	
-
-
-
-
 
