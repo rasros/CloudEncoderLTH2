c0425875448afd65f586e387f1c63cd2f3785ebe
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-06-21 15:01:38 +0200

    added queue time output to wlgen.py

--- wlgen/wlgen.py
+++ wlgen/wlgen.py
@@ -17,7 +17,7 @@ def genWL(base):
 def genWL1(base):
     m = MultipartEncoder(fields={ 'file' :
         ('filename', open('video.mp4', 'rb'), 'video/mp4')})
-    startTime = time.time()
+    queueTime = time.time()
     r = requests.post(base,data = m,
             headers={'Content-Type': m.content_type})
     if r.status_code == 500:
@@ -26,6 +26,10 @@ def genWL1(base):
     file_url = r.headers["Location"]
     r2 = requests.get(file_url + "/status")
     status = r2.json()["status"]
+    
+    while ( status == "QUEUED" ):
+        time.sleep(1)
+    startTime = time.time()
     while ( status != "DONE" ):
         time.sleep(1)
         r2 = requests.get(file_url + "/status")
@@ -34,12 +38,13 @@ def genWL1(base):
 #    r3 = requests.get(file_url + "/download")
 #    theFile = open('tmp.mp4','wb')
 #    theFile.write(r3.content)
-    print(file_url + "," + str(time.time()-startTime) )
+    print(file_url + "," str(startTime-queueTime) + "," + str(time.time()-startTime))
 
 
 
 if __name__ == '__main__':
-    for i in range(1):
+    print("URL,QueueTime,ProcessingTime")
+    for i in range(10):
         t = threading.Thread(target = genWL , args = ["http://transcode.thefuturenow.se:5000/"])
         t.start()
         st = random.randint(1,10)
