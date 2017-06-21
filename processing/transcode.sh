d61f902031fd289b745c82256470d8fa9922b155
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-06-21 14:12:45 +0200

    fixed sound for transcoding

--- processing/transcode.sh
+++ processing/transcode.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-mencoder "$1/in.mp4" -o "$1/out.mp4" '-ovc' 'x264' '-x264encopts' 'crf=20' '-vf' 'scale=1920:1080' '-oac' 'lavc' '-lavcopts' 'abitrate=256' '-srate' '48000' '-channels' '2' 1> "$1/stdout.txt" 2> "$1/stderr.txt"
+mencoder "$1/in.mp4" -o "$1/out.mp4" '-ovc' 'x264' '-x264encopts' 'crf=20' '-vf' 'scale=1080:720' '-oac' 'copy' -fafmttag 0x706D -of 'lavf' 1> "$1/stdout.txt" 2> "$1/stderr.txt"
